bea56507cd531c3d8fc899efbc29af3757a8c4f1
high_haseeb <haseebkhalidoriginal@gmail.com>
2023-06-26 13:31:50 +0500

    another one

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
-// convert jsx element to VDOM element.
-// VDOM element is just a object
-// with three keys: type, props, children
-// props is also an object
-// children is an array
-const createElement = (type, props, ...children) => {
-  props = props || {};
-  return { type, props, children };
+/** @jsx plainReact.createElement */
+const plainReact = {
+  createElement,
+  createTextElement,
+  render,
 };
-let element = <h1 id="foo">"hello world!"</h1>;
+const render = (element, container) => {
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .foreach((name) => {
+      dom[name] = element.props[name];
+    });
+  element.props.children.forEach((child) => {
+    render(child, dom);
+  });
+  container.appendChild(dom);
+};
+const createElement = (type, props, ...children) => {
+  return {
+    type: type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+};
+const createTextElement = (text) => {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+};
+const container = document.getElementById("root");
+const title = document.createElement("h1");
+title.innerText = "it works";
+container.appendChild(title)
+let element = <h1 id="foo">"eid mubarak"</h1>;
+plainReact.render(element, container);
